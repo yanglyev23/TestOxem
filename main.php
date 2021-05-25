699a30c627c824b3cd97bfd188ab87f4ed279cbd
yanglyev23 <yanglyev23@icloud.com>
2021-05-26 01:33:53 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+class animal{
+    public $id;
+}
+
+class chicken extends animal{
+    public function collectEggs{
+
+    }
+}
+
+class cow extends animal{
+    public function collectMilk{
+        
+    }
+}
+?>
