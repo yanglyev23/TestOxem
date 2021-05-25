4dc4d6accfe39c885df0fff200e5ea9ed90b625c
yanglyev23 <yanglyev23@icloud.com>
2021-05-26 01:59:17 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,17 +1,42 @@
 <?php
 class animal{
     public $id;
+    public $temp = 0;
 }
 
 class chicken extends animal{
-    public function collectEggs{
-
+    public function __construct() {
+        $this->id=parent::$temp++;
+    }
+    public function collectEggs(){
+        return rand(0,1);
     }
 }
 
 class cow extends animal{
-    public function collectMilk{
-        
+    function __construct() {
+        $this->id=parent::$temp++;
     }
+    public function collectMilk(){
+        return rand(8,12);
+    }
+}
+
+class Farm{
+    public function createChicken(){
+        return new chicken;
+    }
+    public function createCow(){
+        return new cow;
+    }
+}
+
+$Farm = new Farm();
+$Animals = array();
+for ($i=0; $i<10; $i++){
+    $Animals = $Farm -> createCow();
+}
+for ($i=0; $i<20; $i++){
+    $Animals = $Farm -> createChicken();
 }
 ?>
